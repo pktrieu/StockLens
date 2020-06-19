26fb1191759a20bbaf7302fc0230483702f0c635
Preston <ptrieu96@gmail.com>
2020-06-19 13:09:13 -0700

    Changed ECMAScript target version to 'ESNEXT'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-"use strict";
+const importDefault = (this && this.importDefault) || function (mod) {
+  return (mod && mod.esModule) ? mod : { default: mod };
+};
+Object.defineProperty(exports, '__esModule', { value: true });
+const express1 = importDefault(require('express'));
+
+const app = express1.default();
+app.listen(3001, () => {
+  console.log('Server running...');
+});
